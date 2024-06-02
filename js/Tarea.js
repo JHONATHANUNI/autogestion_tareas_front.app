e0d9e8242f0d9f83685ea6319ac8305abeba6790
JHONATHANUNI <jfuni@uniboyaca.edu.co>
2024-06-02 13:50:19 -0500

    Frontend completamente termiando si faltan cosas se arreglan y se suben despues hasta el momento todo funciona correctamente

--- js/Tarea.js
+++ js/Tarea.js
@@ -1,49 +1,77 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const apiBaseUrl = 'http://127.0.0.1:8000/api';
+    
     const loadEmployees = () => {
-        fetch('/api/empleados')
-            .then(response => response.json())
+        fetch(`${apiBaseUrl}/empleados`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error al cargar empleados: ' + response.statusText);
+                }
+                return response.json();
+            })
             .then(data => {
-                const employeeSelect = document.getElementById('employee');
-                const filterEmployeeSelect = document.getElementById('filter-employee');
-                employeeSelect.innerHTML = '<option value="">Seleccione Responsable</option>';
-                filterEmployeeSelect.innerHTML = '<option value="">Filtrar por Responsable</option>';
-                data.forEach(employee => {
-                    const option = document.createElement('option');
-                    option.value = employee.id;
-                    option.textContent = employee.nombre;
-                    employeeSelect.appendChild(option);
-                    filterEmployeeSelect.appendChild(option.cloneNode(true));
-                });
+                if (Array.isArray(data)) {
+                    const employeeSelect = document.getElementById('employee');
+                    const filterEmployeeSelect = document.getElementById('filter-employee');
+                    employeeSelect.innerHTML = '<option value="">Seleccione Responsable</option>';
+                    filterEmployeeSelect.innerHTML = '<option value="">Filtrar por Responsable</option>';
+                    data.forEach(employee => {
+                        const option = document.createElement('option');
+                        option.value = employee.id;
+                        option.textContent = employee.nombre;
+                        employeeSelect.appendChild(option);
+                        filterEmployeeSelect.appendChild(option.cloneNode(true));
+                    });
+                } else {
+                    console.error('Error al cargar empleados: La respuesta del servidor no es un array');
+                }
             })
             .catch(error => console.error('Error al cargar empleados:', error));
     };
 
+
+
     const loadTasks = () => {
-        fetch('/api/tareas')
-            .then(response => response.json())
+        fetch(`${apiBaseUrl}/tareas`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error al cargar tareas: ' + response.statusText);
+                }
+                return response.json();
+            })
             .then(data => {
-                const tasksTableBody = document.getElementById('tasks-table-body');
-                tasksTableBody.innerHTML = '';
-                data.forEach(task => {
-                    const row = document.createElement('tr');
-                    row.innerHTML = `
-                        <td>${task.título}</td>
-                        <td>${task.descripción}</td>
-                        <td>${task.fechaEstimadaFinalizacion}</td>
-                        <td>${task.prioridad.nombre}</td>
-                        <td>${task.estado.nombre}</td>
-                        <td>${task.empleado.nombre}</td>
-                        <td>
-                            <button class="edit-button" data-id="${task.id}">Editar</button>
-                            <button class="delete-button" data-id="${task.id}">Eliminar</button>
-                        </td>
-                    `;
-                    tasksTableBody.appendChild(row);
-                });
-                addTaskEventListeners();
+                if (Array.isArray(data.data)) {
+                    const tasksTableBody = document.getElementById('tasks-table-body');
+                    if (tasksTableBody) {
+                        tasksTableBody.innerHTML = '';
+                        data.data.forEach(task => {
+                            const row = document.createElement('tr');
+                            row.innerHTML = `
+                            <td>${task.titulo}</td>
+                            <td>${task.descripcion}</td>
+                            <td>${task.fechaEstimadaFinalizacion}</td>
+                            <td>${task.prioridad.nombre}</td>
+                            <td>${task.estado.nombre}</td>
+                            <td>${task.empleado.nombre}</td>
+                            <td>
+                                <button class="edit-button" data-id="${task.id}">Editar</button>
+                                <button class="delete-button" data-id="${task.id}">Eliminar</button>
+                            </td>
+                        `;
+                            tasksTableBody.appendChild(row);
+                        });
+                        addTaskEventListeners();
+                    }
+                } else {
+                    console.error('Error al cargar tareas: La respuesta del servidor no es un array');
+                }
             })
             .catch(error => console.error('Error al cargar tareas:', error));
     };
+
+
+
+
 
     const addTaskEventListeners = () => {
         document.querySelectorAll('.edit-button').forEach(button => {
@@ -55,49 +83,159 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
+
     const handleCreateTask = event => {
         event.preventDefault();
         const taskData = {
-            título: document.getElementById('title').value,
-            descripción: document.getElementById('description').value,
+            titulo: document.getElementById('title').value,
+            descripcion: document.getElementById('description').value,
             fechaEstimadaFinalizacion: document.getElementById('estimated-end-date').value,
             creadorTarea: document.getElementById('creator').value,
-            IdEmpleado: document.getElementById('employee').value,
-            IdEstado: document.getElementById('status').value,
-            IdPrioridad: document.getElementById('priority').value,
+            idEmpleado: document.getElementById('employee').value,
+            idEstado: document.getElementById('status').value,
+            idPrioridad: document.getElementById('priority').value,
             observaciones: document.getElementById('observations').value
         };
-        fetch('/api/tareas', {
+        fetch(`${apiBaseUrl}/tareas`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(taskData)
         })
-        .then(response => response.json())
-        .then(() => {
+        .then(response => response.json().then(data => ({ status: response.status, body: data })))
+        .then(({ status, body }) => {
+            if (status !== 201) {
+                console.error('Error al crear tarea:', body);
+                throw new Error(`Error al crear tarea: ${status} - ${JSON.stringify(body)}`);
+            }
             loadTasks();
             document.getElementById('create-task-form').reset();
         })
-        .catch(error => console.error('Error al crear tarea:', error));
+        .catch(error => console.error(error));
     };
+    
+
+
+
+
 
     const handleEditTask = event => {
         const taskId = event.target.dataset.id;
-        // Implementa la lógica para editar la tarea
-        console.log('Editar tarea:', taskId);
+        fetch(`${apiBaseUrl}/tareas/${taskId}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error al cargar tarea: ' + response.statusText);
+                }
+                return response.json();
+            })
+            .then(data => {
+                // Rellena el formulario con los datos actuales de la tarea
+                document.getElementById('title').value = data.titulo;
+                document.getElementById('description').value = data.descripcion;
+                document.getElementById('estimated-end-date').value = data.fechaEstimadaFinalizacion;
+                document.getElementById('creator').value = data.creadorTarea;
+                document.getElementById('employee').value = data.idEmpleado;
+                document.getElementById('status').value = data.idEstado;
+                document.getElementById('priority').value = data.idPrioridad;
+                document.getElementById('observations').value = data.observaciones;
+
+                // Cambia el manejador del evento de envío del formulario para actualizar la tarea en lugar de crear una nueva
+                document.getElementById('create-task-form').removeEventListener('submit', handleCreateTask);
+                document.getElementById('create-task-form').addEventListener('submit', event => {
+                    event.preventDefault();
+                    const taskData = {
+                        titulo: document.getElementById('title').value,
+                        descripcion: document.getElementById('description').value,
+                        fechaEstimadaFinalizacion: document.getElementById('estimated-end-date').value,
+                        creadorTarea: document.getElementById('creator').value,
+                        idEmpleado: document.getElementById('employee').value,
+                        idEstado: document.getElementById('status').value,
+                        idPrioridad: document.getElementById('priority').value,
+                        observaciones: document.getElementById('observations').value
+                    };
+                    fetch(`${apiBaseUrl}/tareas/${taskId}`, {
+                        method: 'PUT',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(taskData)
+                    })
+                        .then(response => {
+                            if (!response.ok) {
+                                throw new Error('Error al actualizar tarea: ' + response.statusText);
+                            }
+                            return response.json();
+                        })
+                        .then(() => {
+                            loadTasks();
+                            document.getElementById('create-task-form').reset();
+
+                            // Cambia el manejador del evento de envío del formulario de vuelta a handleCreateTask
+                            document.getElementById('create-task-form').removeEventListener('submit', handleUpdateTask);
+                            document.getElementById('create-task-form').addEventListener('submit', handleCreateTask);
+                        })
+                        .catch(error => console.error(error.message));
+                }, { once: true });
+            })
+            .catch(error => console.error('Error al cargar tarea:', error));
     };
+
+
+
+    const handleUpdateTask = event => {
+        event.preventDefault();
+        const taskId = event.target.dataset.id;
+        const taskData = {
+            titulo: document.getElementById('title').value,
+            descripcion: document.getElementById('description').value,
+            fechaEstimadaFinalizacion: document.getElementById('estimated-end-date').value,
+            creadorTarea: document.getElementById('creator').value,
+            idEmpleado: document.getElementById('employee').value,
+            idEstado: document.getElementById('status').value,
+            idPrioridad: document.getElementById('priority').value,
+            observaciones: document.getElementById('observations').value
+        };
+        fetch(`${apiBaseUrl}/tareas/${taskId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(taskData)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error al actualizar tarea: ' + response.statusText);
+                }
+                return response.json();
+            })
+            .then(() => {
+                loadTasks();
+                document.getElementById('create-task-form').reset();
+                // Cambia el manejador del evento de envío del formulario de vuelta a handleCreateTask
+                document.getElementById('create-task-form').removeEventListener('submit', handleUpdateTask);
+                document.getElementById('create-task-form').addEventListener('submit', handleCreateTask);
+            })
+            .catch(error => console.error(error.message));
+    };
+
+
 
     const handleDeleteTask = event => {
         const taskId = event.target.dataset.id;
-        fetch(`/api/tareas/${taskId}`, {
+        fetch(`${apiBaseUrl}/tareas/${taskId}`, {
             method: 'DELETE'
         })
-        .then(() => {
-            loadTasks();
-        })
-        .catch(error => console.error('Error al eliminar tarea:', error));
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error al eliminar tarea: ' + response.statusText);
+                }
+                loadTasks(); // Actualizar la lista de tareas después de eliminar una
+            })
+            .catch(error => console.error('Error al eliminar tarea:', error));
     };
+
+
 
     const handleFilterTasks = () => {
         const title = document.getElementById('search-title').value;
@@ -106,39 +244,46 @@ document.addEventListener('DOMContentLoaded', () => {
         const priority = document.getElementById('filter-priority').value;
         const status = document.getElementById('filter-status').value;
         const employee = document.getElementById('filter-employee').value;
-
-        let url = '/api/tareas?';
-        if (title) url += `título=${title}&`;
+    
+        let url = `${apiBaseUrl}/tareas?`;
+        if (title) url += `titulo=${title}&`;
         if (startDate) url += `startDate=${startDate}&`;
         if (endDate) url += `endDate=${endDate}&`;
-        if (priority) url += `IdPrioridad=${priority}&`;
-        if (status) url += `IdEstado=${status}&`;
-        if (employee) url += `IdEmpleado=${employee}&`;
-
+        if (priority) url += `idPrioridad=${priority}&`;
+        if (status) url += `idEstado=${status}&`;
+        if (employee) url += `idEmpleado=${employee}&`;
+    
         fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                const tasksTableBody = document.getElementById('tasks-table-body');
-                tasksTableBody.innerHTML = '';
-                data.forEach(task => {
-                    const row = document.createElement('tr');
-                    row.innerHTML = `
-                        <td>${task.título}</td>
-                        <td>${task.descripción}</td>
-                        <td>${task.fechaEstimadaFinalizacion}</td>
-                        <td>${task.prioridad.nombre}</td>
-                        <td>${task.estado.nombre}</td>
-                        <td>${task.empleado.nombre}</td>
-                        <td>
-                            <button class="edit-button" data-id="${task.id}">Editar</button>
-                            <button class="delete-button" data-id="${task.id}">Eliminar</button>
-                        </td>
-                    `;
-                    tasksTableBody.appendChild(row);
-                });
-                addTaskEventListeners();
-            })
-            .catch(error => console.error('Error al filtrar tareas:', error));
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al filtrar tareas');
+            }
+            return response.json();
+        })
+        .then(responseData => {
+            const data = responseData.data; // Accedemos a la propiedad 'data' del objeto de respuesta
+            const tasksTableBody = document.getElementById('tasks-table-body');
+            tasksTableBody.innerHTML = '';
+            data.forEach(task => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${task.titulo}</td>
+                    <td>${task.descripcion}</td>
+                    <td>${task.fechaEstimadaFinalizacion}</td>
+                    <td>${task.prioridad.nombre}</td>
+                    <td>${task.estado.nombre}</td>
+                    <td>${task.empleado.nombre}</td>
+                    <td>
+                        <button class="edit-button" data-id="${task.id}">Editar</button>
+                        <button class="delete-button" data-id="${task.id}">Eliminar</button>
+                    </td>
+                `;
+                tasksTableBody.appendChild(row);
+            });
+            addTaskEventListeners();
+        })
+        .catch(error => console.error(error.message));
+        
     };
 
     document.getElementById('create-task-form').addEventListener('submit', handleCreateTask);
